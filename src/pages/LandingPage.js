cbdfcf4b58b82d998e8292c362175cad1e97aef9
kimpeep <rnunseoo@gmail.com>
2022-08-28 02:25:18 +0900

    base work

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return(
+        <div className='landing'>
+        LandingPage
+        </div>
+    )
+
+}
+
+export default LandingPage
