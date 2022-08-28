8559ec31da36524956c3d32170253b19764bc0c1
kimpeep <rnunseoo@gmail.com>
2022-08-28 18:49:33 +0900

    fix: Detail

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import "../css/DetailPage.css"
 
 const DetailPage = (props) => {
     return(
         <div className='detail'>
-        00000전공을 선택한 사람은 다음과 같은 비율이에요!
+            <div className='text'>
+            00000전공을 선택한 사람은 다음과 같은 비율이에요!
+            </div>
 
-        더 자세한 정보를 원한다면?
-
+        <NavLink to ="/collaboration">
+            <button className='button'>
+                더 자세한 정보를 원한다면?
+            </button>
+        </NavLink>
         </div>
     )
 }
