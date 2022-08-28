338e7df220d384d87476291730f67fbefe812a64
kimpeep <rnunseoo@gmail.com>
2022-08-28 16:24:17 +0900

    fix: LandingPage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,7 +4,22 @@ import "../css/LandingPage.css"
 const LandingPage = () => {
     return(
         <div className='landing'>
-        Linking at Duksung
+            <div className='Text'>
+                <div className='bigText'> 
+                    Linking 
+                </div>
+                <div className='smallText'>
+                    <p>Global Convergence</p>
+                    <p>Science&Technology</p>
+                    <p>Art&Design</p>
+                </div>
+                <div className='bigText'> 
+                    at Duksung!
+                </div>
+            </div>
+            <button className="landingButton">
+                Data UnLock!
+            </button>
         </div>
     )
 
