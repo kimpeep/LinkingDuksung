0c3b20806b2926b5d09068062acdda5dfc14843f
tndus03 <tndus_03@naver.com>
2022-08-29 00:15:24 +0900

    UnlockPage error

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import "../css/DetailPage.css"
 
 const DetailPage = (props) => {
+    const {state} = useLocation()
+    const major = {state}
+
     const first = 750*0.63
     const second = 750*0.27
     return(
         <div className='detail'>
             <div className='text'>
-            00000전공을 선택한 사람은 다음과 같은 비율이에요!
+                {console.log(major)}
+            전공을 선택한 사람은 다음과 같은 비율이에요!
             </div>
             <div className='graph'>
                 <div className='circle' style={{width: first, height: first}}>
--- src/pages/UnlockPage.js
+++ src/pages/UnlockPage.js
@@ -1,29 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import "../css/UnlockPage.css";
 import GCData from "../data/GCData.json";
 import STData from "../data/STData.json";
 import ADData from "../data/ADData.json";
-import { Grid, Box } from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
 
 const UnlockPage = () => {
-    // const [ss, setSs] = useState(true)
-    const [num, setNum] = useState(0)
+    const navigate = useNavigate();
 
-    // const dd = () => {
-    //     if (num < 23) {
-    //         setNum(num + 1)
-    //         console.log("true" + num)
-    //         console.log(GCData.data[num].major)
-    //         return <p>{GCData.data[num].major}</p>
-    //     }
-    // }
-    
+    const globalClick = (e) => {
+        navigate("/dataunlock/detail", {state: {major: "바보"}});
+        // console.log(e.target.value)
+    }
+
     return(
         <>
             <div className="box">
                 <div className="collage">
                     <p className="globalCollageText">Global<br />Convergence</p>
-                    <p className="majorText">{GCData.data.map(data => {return <p>{data.major}&nbsp;&nbsp;&nbsp;</p>})}</p>
+                    <p className="majorText">{GCData.data.map(data => {return <button onClick={(globalClick())} className="majorButton">{data.major}&nbsp;&nbsp;&nbsp;</button>})}</p>
                 </div>
                 <div className="collage">
                     <p className="collageText">Science&<br />Technology</p>
@@ -31,7 +26,6 @@ const UnlockPage = () => {
                 </div>
                 <div className="collage">
                     <p className="collageText">Art&Design</p>
-                    {/* <p className="majorText">{ADData.data.map(data => {return <p>{data.major}&nbsp;&nbsp;&nbsp;</p>})}</p> */}
                     <p className="text">{ADData.data.map(data => {return <p>{data.major}&nbsp;&nbsp;&nbsp;</p>})}</p>
                 </div>
             </div>
