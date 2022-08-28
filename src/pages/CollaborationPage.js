6287671ea5a6939b9890b91223e65d007c7e47c4
won0324 <huiwon0324@naver.com>
2022-08-28 21:58:43 +0900

    fix: CollaborationPage

--- src/pages/CollaborationPage.js
+++ src/pages/CollaborationPage.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import "../css/CollaborationPage.css"
+import React from "react";
+import "../css/CollaborationPage.css";
+import majorData from "../data/majorData.json";
 
 const CollaborationPage = () => {
-    return(
-        <>
-        </>
-    )
-}
+  const handleChange = (e) => {
+    //console.log(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="CollaborationPage" onChange={handleChange}>
+        <div className="select-wrapper">
+          <select>
+            {majorData &&
+              majorData.major.map((mdata) => (
+                <option key={mdata._id} value={mdata.type}>
+                  {mdata.type}
+                </option>
+              ))}
+          </select>
+          <p>과</p>
+          <select>
+            {majorData &&
+              majorData.major.map((mdata) => (
+                <option key={mdata._id} value={mdata.type}>
+                  {mdata.type}
+                </option>
+              ))}
+          </select>
+          <p>를 선택한 사람은?</p>
+        </div>
+        <div className="type-result">
+          <p>
+            총 <span>0</span> 명이에요!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default CollaborationPage;
