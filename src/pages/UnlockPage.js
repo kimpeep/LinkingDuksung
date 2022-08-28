b14dc1d8bd291075b66ff14d3f78ad085286ee7e
tndus03 <tndus_03@naver.com>
2022-08-28 22:50:13 +0900

    fix: UnlockPage

--- src/pages/UnlockPage.js
+++ src/pages/UnlockPage.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../css/UnlockPage.css";
+import GCData from "../data/GCData.json";
+import STData from "../data/STData.json";
+import ADData from "../data/ADData.json";
+import { Grid, Box } from '@material-ui/core';
 
 const UnlockPage = () => {
+    // const [ss, setSs] = useState(true)
+    const [num, setNum] = useState(0)
+
+    // const dd = () => {
+    //     if (num < 23) {
+    //         setNum(num + 1)
+    //         console.log("true" + num)
+    //         console.log(GCData.data[num].major)
+    //         return <p>{GCData.data[num].major}</p>
+    //     }
+    // }
+    
     return(
         <>
+            <div className="box">
+                <div className="collage">
+                    <p className="globalCollageText">Global<br />Convergence</p>
+                    <p className="majorText">{GCData.data.map(data => {return <p>{data.major}&nbsp;&nbsp;&nbsp;</p>})}</p>
+                </div>
+                <div className="collage">
+                    <p className="collageText">Science&<br />Technology</p>
+                    <p className="majorText">{STData.data.map(data => {return <p>{data.major}&nbsp;&nbsp;&nbsp;</p>})}</p>
+                </div>
+                <div className="collage">
+                    <p className="collageText">Art&Design</p>
+                    {/* <p className="majorText">{ADData.data.map(data => {return <p>{data.major}&nbsp;&nbsp;&nbsp;</p>})}</p> */}
+                    <p className="text">{ADData.data.map(data => {return <p>{data.major}&nbsp;&nbsp;&nbsp;</p>})}</p>
+                </div>
+            </div>
         </>
     )
 }
