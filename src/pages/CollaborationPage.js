0bc9eed87938fca1a6ca676f7c8a18ee4c167ba8
sky <shn7446@duksung.ac.kr>
2022-08-29 00:16:30 +0900

    fix: CollaborationPage

--- src/pages/CollaborationPage.js
+++ src/pages/CollaborationPage.js
@@ -2,10 +2,19 @@ import React from "react";
 import "../css/CollaborationPage.css";
 import search from "../img/search.png";
 import majorData from "../data/majorData.json";
+import { useState } from "react";
 
 const CollaborationPage = () => {
+  const [total, setTotal] = useState(0);
+
   const handleChange = (e) => {
-    //console.log(e.target.value);
+    console.log(e.target.value);
+    // ['국어국문학전공', '영어영문학전공'] => 배열.includes('국어국문학전공')
+    // setTotal 변경
+  };
+
+  const handleClick = (e) => {
+    setTotal(3);
   };
 
   return (
@@ -13,6 +22,9 @@ const CollaborationPage = () => {
       <div className="CollaborationPage" onChange={handleChange}>
         <div className="select-wrapper">
           <select>
+            <option value={"none"} disabled>
+              1전공
+            </option>
             {majorData &&
               majorData.major.map((mdata) => (
                 <option key={mdata._id} value={mdata.type}>
@@ -22,6 +34,9 @@ const CollaborationPage = () => {
           </select>
           <p>과</p>
           <select>
+            <option value={"none"} disabled>
+              2전공
+            </option>
             {majorData &&
               majorData.major.map((mdata) => (
                 <option key={mdata._id} value={mdata.type}>
@@ -32,7 +47,7 @@ const CollaborationPage = () => {
           <p>을 선택한 사람은?</p>
         </div>
         <div className="search-box">
-          <button className="search-btn">
+          <button className="search-btn" onClick={handleClick}>
             <img
               id="search-img"
               src={search}
@@ -45,7 +60,7 @@ const CollaborationPage = () => {
         </div>
         <div className="type-result" id="type-result">
           <p>
-            총 <span>0</span> 명이에요!
+            총 <span>{total}</span> 명이에요!
           </p>
         </div>
       </div>
